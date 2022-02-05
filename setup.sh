beba461ce9b2ca6bd01c7117682e23c79751a924
SaltyCodeMaker <69536211+SaltyCodeMaker@users.noreply.github.com>
2022-02-05 11:47:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,29 +5,20 @@
 sudo apt install apt-transport-https curl
 sudo apt install neofetch snap npm
 sudo add-apt-repository ppa:libreoffice/ppa
-#needed stuff-------------------------------------------------------------------
-
-
-#brave-------------------------------------------------------------------------
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-#brave--------------------------------------------------------------------------
+#needed stuff-------------------------------------------------------------------
 
 
 #install------------------------------------------------------------------------
 sudo apt update
 
-sudo apt install gimp brave-browser zsh libreoffice stacer ranger terminator kdeconnect bat tldr
+sudo apt install gimp brave-browser zsh libreoffice stacer ranger terminator vlc audacity gnome-shell-extensions vim
 
-sudo apt purge fiefox
-
-sudo snap install discord
+sudo snap install discord whatsie
 
 sudo npm install gtop -gi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #install-------------------------------------------------------------------------
-
-xdg-open https://protonvpn.com/support/linux-ubuntu-vpn-setup/
