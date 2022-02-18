7893101fd5eced4aaaf3e78eb11656c936cfd1af
SaltyCodeMaker <69536211+SaltyCodeMaker@users.noreply.github.com>
2022-02-18 20:38:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,19 +2,17 @@
 
 
 #needed stuff-------------------------------------------------------------------
-sudo apt install apt-transport-https curl
-sudo apt install snap npm
-sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt install snap npm -y
+sudo add-apt-repository ppa:libreoffice/ppa ppa:kdenlive/kdenlive-stable .y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo add-apt-repository ppa:kdenlive/kdenlive-stable
 #needed stuff-------------------------------------------------------------------
 
 
 #install------------------------------------------------------------------------
-sudo apt update
+sudo apt update -y
 
-sudo apt install gimp brave-browser zsh libreoffice stacer ranger vlc audacity vim kdenlive neofetch grub-customizer 
+sudo apt install gimp brave-browser zsh libreoffice stacer ranger vlc audacity vim kdenlive neofetch grub-customizer -y
 
 sudo snap install discord whatsie
 
