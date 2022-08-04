7eaba7472593b3e31c75c392717e04923cec0bdb
SaltyCodeMaker <69536211+SaltyCodeMaker@users.noreply.github.com>
2022-08-04 15:27:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo pacman -S --quiet zsh firefox vim discord konsole rpm
+sudo pacman -S -q zsh firefox vim discord konsole rpm htop neofetch
 
 sudo rpm install gtop
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
 chsh /bin/zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo cp /home/$USER/Salty/.zshrc /home/$USER
+sudo cp /home/$USER/Salty/salty.zsh-theme /home/$USER/.oh-my-zsh/themes
