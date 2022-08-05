be6370b56ad6a170965a1539787b4b2ce8c1e31f
SaltyCodeMaker <69536211+SaltyCodeMaker@users.noreply.github.com>
2022-08-06 01:43:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 
 #!/bin/bash
 
+cd ~
+
 #yay------------------------------------------------------
 sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
+sudo git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ~
