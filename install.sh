a6c2dd4e541961d89bf248d4b8bfca67244260a4
SaltyIceteaMaker <69536211+SaltyIceteaMaker@users.noreply.github.com>
2023-05-05 19:03:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,31 @@
-
 #!/bin/bash
 
+USER=$( whoami )
+
 cd ~
+
+pacman -Syy
 
 #yay------------------------------------------------------
 sudo pacman -S -q --needed git base-devel
 sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R salty:salty ./yay
+sudo chown -R $USER:$USER ./yay
 cd yay
 makepkg -si
 cd ~
+yay -Syy
 #yay------------------------------------------------------
 
-sudo pacman -S -q zsh firefox vim discord htop neofetch lolcat steam flatpak yakuake vlc lsd figlet
+sudo pacman -S -q zsh firefox vim nvim discord htop neofetch lolcat steam flatpak yakuake vlc lsd figlet wget
 
-yay -Syu visual-studio-code-bin
+#fonts----------------------------------------------------
+cd ~/Downloads 
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/JetBrainsMono.zip
+sudo mkdir /usr/local/share/fonts && sudo unzip JetBrainsMono.zip /usr/local/share/fonts/
+cd ~
+#fonts----------------------------------------------------
+
+yay -S visual-studio-code-bin
  
 sudo flatpak install spotify
 
@@ -25,6 +36,4 @@ sudo chsh /usr/bin/zsh
 sudo cp /home/$USER/Salty/.zshrc /home/$USER
 sudo cp /home/$USER/Salty/salty.zsh-theme /home/$USER/.oh-my-zsh/themes
 sudo cp /home/$USER/Salty/yakuakerc /home/$USER/.config
-
-lookandfeeltool -a org.kde.breezedark.desktop
 
